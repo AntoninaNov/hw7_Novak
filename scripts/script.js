4a543f5ad19b56bc2a0d6eb3ca157ce050a2e4f6
AntNov <tonianovak@yahoo.com>
2023-10-12 14:10:19 +0300

    Kinda starting point

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const todoList = document.getElementById('todoList');
+    const newTaskInput = document.getElementById('newTask');
+    const addTaskButton = document.getElementById('addTask');
+
+    // TODO: Add event listeners for functionalities like add, remove, etc.
+
+    function addTask() {
+    }
+
+    // TODO: Implement other functionalities like remove, toggle, update, etc.
+});
